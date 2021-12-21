6a101ad0df027f1f35b1ff98211764bcfdfc29fd
Igor Nascimento <64155074+IgorNCS@users.noreply.github.com>
2021-12-21 17:55:44 -0300

    Add files via upload

---
+++ BD/bd_filando_preestabelecimento.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE  IF NOT EXISTS `bd_filando` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `bd_filando`;
+-- MariaDB dump 10.19  Distrib 10.4.21-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: bd_filando
+-- ------------------------------------------------------
+-- Server version	10.4.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `preestabelecimento`
+--
+
+DROP TABLE IF EXISTS `preestabelecimento`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `preestabelecimento` (
+  `idpreestabelecimento` int(11) NOT NULL AUTO_INCREMENT,
+  `prenome` varchar(255) NOT NULL,
+  `prelocal` varchar(1000) NOT NULL,
+  `pretipo` varchar(255) NOT NULL,
+  `preimage` varchar(1000) NOT NULL,
+  PRIMARY KEY (`idpreestabelecimento`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `preestabelecimento`
+--
+
+LOCK TABLES `preestabelecimento` WRITE;
+/*!40000 ALTER TABLE `preestabelecimento` DISABLE KEYS */;
+INSERT INTO `preestabelecimento` VALUES (9,'Outback Steakhouse Shopping da Bahia','Shopping da Bahia, Av. Tancredo Neves, 148 - Caminho das Árvores, Salvador - BA, 41820-908','restaurante','https://media-cdn.tripadvisor.com/media/photo-s/10/ae/97/dc/outback-steakhouse-iguatemi.jpg');
+/*!40000 ALTER TABLE `preestabelecimento` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-29 19:04:45
